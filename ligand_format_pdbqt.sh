3da159c00b688aafbebacbd7e15bd3069ca5dd91
Sukanya Konar <87675359+sukanyabiochem@users.noreply.github.com>
2026-05-18 17:30:36 -0500

    Add files via upload

---
+++ ligand_format_pdbqt.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+##This generates:
+## ligand_1.pdb
+##ligand_2.pdb
+##ligand_3.pdb
+##...
+
+for num in {1..34}
+do
+mkdir -p ligand_prep ; ## create folder to make library of the ligand ## 
+obabel ligand_${num}.pdb -O ligand_prep/ligand_${num}.mol2 -h ; ## convert the pdb to mol2 file ##    	
+mk_prepare_ligand.py -i ligand_prep/ligand_${num}.mol2 -o ligand_prep/ligand_${num}.pdbqt ; ## Convertion of the ligand format to autodoc compatable pdbqt ##  
+echo "$num"
+done
