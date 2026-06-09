723cfce9c45506ebd98672bc6a4d1007bbf6b168
Sukanya Konar <87675359+sukanyabiochem@users.noreply.github.com>
2026-06-09 14:52:18 -0500

    Update ligand_format_pdbqt.sh

--- ligand_format_pdbqt.sh
+++ ligand_format_pdbqt.sh
@@ -9,7 +9,8 @@
 for num in {1..34}
 do
 mkdir -p ligand_prep ; ## create folder to make library of the ligand ## 
-obabel ligand_${num}.pdb -O ligand_prep/ligand_${num}.mol2 -h ; ## convert the pdb to mol2 file ##    	
+obabel ligand_${num}.pdb -O ligand_prep/ligand_${num}.mol2 -h ; ## convert the pdb to mol2 file ##  
+obabel ../ligand_${num}.pdb -O ../ligand_${num}.sdf ## convert the pdb to sdf file format ##
 mk_prepare_ligand.py -i ligand_prep/ligand_${num}.mol2 -o ligand_prep/ligand_${num}.pdbqt ; ## Convertion of the ligand format to autodoc compatable pdbqt ##  
 echo "$num"
 done
